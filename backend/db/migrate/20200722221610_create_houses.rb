c5594a6c5df01f7fa5c78040ea6856f33e69cce6
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-22 23:20:12 +0100

    added migrations, model, controllers for user and house

---
+++ backend/db/migrate/20200722221610_create_houses.rb
@@ -0,0 +1,12 @@
+class CreateHouses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :houses do |t|
+      t.string :name
+      t.integer :house_coordinates
+      t.string :house_information
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
