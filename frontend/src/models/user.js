7af576f953cd62323a7093062b3825cd0dc43960
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-24 11:19:38 +0100

    added script tags to html for api class and user class, added get all users fucntion

--- frontend/src/models/user.js
+++ frontend/src/models/user.js
@@ -2,4 +2,10 @@ class User {
   constructor(name, home_coordinates) {
     (this.name = name), (this.home_coordinates = home_coordinates);
   }
+
+  static getAllUsers() {
+    API.get("/users").then(user => {
+      console.log(user);
+    });
+  }
 }
