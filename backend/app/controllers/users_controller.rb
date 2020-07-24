36b8a49487e57f204a87ee6788909a731b52a111
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-24 10:55:35 +0100

    added user controller index, show, create

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users, only: [:id, :name, :home_coordinates]
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        houses = user.houses
+
+        render json: user, include: [:houses]
+    end
+
+    def create
+        user = User.create(params)
+    end
 end
