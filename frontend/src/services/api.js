e29d5076a349b400e0e8db33d81b4ffa0a84b2ce
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-24 17:08:40 +0100

    added api service post fetch request

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -10,5 +10,14 @@ class API {
     });
   }
 
-  static post(url, data) {}
+  static post(url, data) {
+    return fetch(this.baseURL + url, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }).then(response => response.json());
+  }
 }
