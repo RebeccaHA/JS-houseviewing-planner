cf1f736441bf07f602ab0d250b076e7f0ddfdd9c
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-26 12:46:05 +0100

    updated seed data to switch longitude and latitude

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,12 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: "Ben", home_longitude: 51.5074, home_latitude: -0.1278)
-User.create(name: "Brain",home_longitude: 51.4880,home_latitude: -0.1119)
-User.create(name: "Borris",home_longitude: 51.3981,home_latitude: -0.0716)
-User.create(name: "Bill",home_longitude: 51.4966,home_latitude: -0.0147)
-User.create(name: "Brice",home_longitude: 51.4518, home_latitude: -0.1252)
-User.create(name: "Bob",home_longitude: 51.5002,home_latitude: 0.1034)
+User.create(name: "Ben", home_latitude: 51.5074, home_longitude: -0.1278)
+User.create(name: "Brain",home_latitude: 51.4880,home_longitude: -0.1119)
+User.create(name: "Borris",home_latitude: 51.3981,home_longitude: -0.0716)
+User.create(name: "Bill",home_latitude: 51.4966,home_longitude: -0.0147)
+User.create(name: "Brice",home_latitude: 51.4518, home_longitude: -0.1252)
+User.create(name: "Bob",home_latitude: 51.5002,home_longitude: 0.1034)
 
 House.create(name: "house 0", lat:51.4613, lng: -0.1156, viewing_time: "13:00", viewing_date: "02/07/2020", house_information:"this is a 4 four bed with some sound people", user_id:1)
 House.create(name: "house 1", lat:51.5390,lng: -0.1426, viewing_time: "13:00", viewing_date: "02/07/2020", house_information:"this is a 4 four bed with some sound people", user_id:2)
