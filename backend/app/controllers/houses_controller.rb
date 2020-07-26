fa9699cb012b95f779c23b5b8202d5ff7697fb3c
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-26 15:59:53 +0100

    added house method create and strong params

--- backend/app/controllers/houses_controller.rb
+++ backend/app/controllers/houses_controller.rb
@@ -1,5 +1,16 @@
 class HousesController < ApplicationController
-   def index
+   def create
+      house = House.new(house_params)
+      user = User.find_by(id: params[:id])
 
+      if house.save
+         render json: house, include: [:user]
+      else 
+         render json: house.errors
+      end
    end
+
+   def house_params
+      params.require(:house).permit(:name, :lat, :lng, :viewing_time, :viewing_date, :house_information)
+  end
 end
---
+++ frontend/src/models/house.js
@@ -0,0 +1,16 @@
+class House {
+  constructor(
+    id,
+    name,
+    lng,
+    lat,
+    viewing_time,
+    viewing_date,
+    house_information
+  ) {
+    (this.id = id),
+      (this.name = name),
+      (this.home_longitude = home_longitude),
+      (this.home_latitude = home_latitude);
+  }
+}
