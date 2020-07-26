dbdbe77d31e94c5cf64a4c0b63bb05086e76c03a
RebeccaHA <rebeccahunt034@gmail.com>
2020-07-26 16:16:57 +0100

    added viewing function and post api, yet to be tested

--- frontend/src/models/house.js
+++ frontend/src/models/house.js
@@ -17,5 +17,38 @@ class House {
       (this.house_information = house_information);
   }
 
-  static addViewing() {}
+  static addViewing() {
+    const name = document.getElementById("house-name").value;
+    const longitude = document.getElementById("lng").value;
+    const latitude = document.getElementById("lat").value;
+    const viewing_time = document.getElementById("viewing-date").value;
+    const viewing_date = document.getElementById("viewing-time").value;
+    const house_information = document.getElementById("house-information");
+    const user_id = document.getElementById("option").value;
+    let house = {
+      name: house - name,
+      lng: longitude,
+      lat: latitude,
+      viewing_time: viewing_time,
+      viewing_date: viewing_date,
+      house_information: house_information
+    };
+
+    API.post("/houses", house).then(data => {
+      let newHouse = new House(
+        data.id,
+        data.name,
+        data.lng,
+        data.lat,
+        data.viewing_time,
+        data.viewing_date,
+        data.house_information
+      );
+
+      let coords{
+          
+      }
+      newHouse.addMarker(newHouse, content);
+    });
+  }
 }
