30357e40b91ea36c65442121c8b821717b335a1a
RebeccaHA <rebeccahunt034@gmail.com>
2020-08-03 23:04:41 +0100

    added patch request, which goes to the addCard function and checks if the house view is set to true, if so updates class to be strikethrough

--- backend/app/controllers/houses_controller.rb
+++ backend/app/controllers/houses_controller.rb
@@ -17,9 +17,26 @@ class HousesController < ApplicationController
       end
    end
 
+   def update
+      house = House.find_by_id(params[:id])
+      
+      if params[:checked] === "true"
+         house.viewed = true
+         
+      else
+         house.viewed = false
+
+      end
+
+      house.save
+
+
+      render json: house
+   end
+
 
 
    def house_params
-      params.require(:house).permit(:name, :lat, :lng, :viewing_time, :viewing_date, :house_information, :user_id)
+      params.require(:house).permit(:name, :lat, :lng, :viewing_time, :viewing_date, :house_information, :user_id, :viewed,:checked)
   end
 end
